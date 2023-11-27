7e763b7e5565ad72c372a23832304583e4b27a97
Sumit Jangid <sumitjangid1692153@gmail.com>
2023-11-27 17:50:03 +0530

    Added : UC2 Calculate Daily Wage of Employee

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -1,5 +1,10 @@
 public class EmployeeWageComputation {
-    public static void checkAttendance(){
+    /*
+        *@desc : Checks the attendance of employee is present of absent
+        *@params :
+        *@return : boolean presentStatus
+     */
+    public static boolean checkAttendance(){
         boolean presentStatus  = (Math.random() < 0.5) ? false : true;
         if(presentStatus){
             System.out.println("Employee is Present");
@@ -7,8 +12,25 @@ public class EmployeeWageComputation {
         else{
             System.out.println("Employee is Absent");
         }
+        return presentStatus;
+    }
+    /*
+     *@desc : Calculated the daily wage of employee
+     *@params : No of hours worked by employee , No of wage per hour
+     *@return : int wage
+     */
+    public static int calculateDailyWage(int hoursWorked , int wagePerHour){
+        return (hoursWorked*wagePerHour);
     }
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+        if(checkAttendance()){
+            int wagePerHour = 20;
+            int hoursWorked = 8;
+            System.out.println("Daily Employee Wage is : " + calculateDailyWage(hoursWorked , wagePerHour));
+        }
+        else{
+            System.out.println("Daily Employee Wage is : 0");
+        }
     }
 }
