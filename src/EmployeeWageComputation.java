d2dd1ca2341e4f8aa010690d2372562fbbf86d13
Sumit Jangid <sumitjangid1692153@gmail.com>
2023-11-27 18:19:58 +0530

    Added : UC3 Add part time employee and wage

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -1,36 +1,45 @@
 public class EmployeeWageComputation {
+    private static final int WAGE_PER_HOUR = 20;
     /*
      *@desc : Checks the attendance of employee is present of absent
      *@params :
      *@return : boolean presentStatus
      */
-    public static boolean checkAttendance(){
-        boolean presentStatus  = (Math.random() < 0.5) ? false : true;
-        if(presentStatus){
-            System.out.println("Employee is Present");
-        }
-        else{
+    private static int checkAttendance(){
+        int employeeAttendance  = (int)(Math.random() * 3)+1;
+        if(employeeAttendance == 1){
             System.out.println("Employee is Absent");
         }
-        return presentStatus;
+        else if(employeeAttendance == 2){
+            System.out.println("Employee is Present and Full Time Employee");
+        }
+        else{
+            System.out.println("Employee is Present and Part Time Employee");
+        }
+        return employeeAttendance;
     }
     /*
      *@desc : Calculated the daily wage of employee
      *@params : No of hours worked by employee , No of wage per hour
      *@return : int wage
      */
-    public static int calculateDailyWage(int hoursWorked , int wagePerHour){
-        return (hoursWorked*wagePerHour);
+    private static int calculateDailyWage(int hoursWorked){
+        return (hoursWorked*WAGE_PER_HOUR);
     }
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
-        if(checkAttendance()){
-            int wagePerHour = 20;
-            int hoursWorked = 8;
-            System.out.println("Daily Employee Wage is : " + calculateDailyWage(hoursWorked , wagePerHour));
+        int hoursWorked;
+        int employeeAttendance = checkAttendance();
+        if(employeeAttendance==1){
+            System.out.println("Daily Employee Wage is : 0");
+        }
+        else if(employeeAttendance==2){
+            hoursWorked = 8;
+            System.out.println("Daily Employee Wage of Full time Employee is : " + calculateDailyWage(hoursWorked));
         }
         else{
-            System.out.println("Daily Employee Wage is : 0");
+            hoursWorked = 4;
+            System.out.println("Daily Employee Wage of Part time Employee is : " + calculateDailyWage(hoursWorked));
         }
     }
 }
