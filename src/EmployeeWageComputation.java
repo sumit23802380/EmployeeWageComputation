e879dc526dfa98dcc01f1f321f3eb942e6d43ff0
Sumit Jangid <sumitjangid1692153@gmail.com>
2023-11-27 17:38:04 +0530

    Added : checkAttendance

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -1,4 +1,13 @@
 public class EmployeeWageComputation {
+    public static void checkAttendance(){
+        boolean presentStatus  = (Math.random() < 0.5) ? false : true;
+        if(presentStatus){
+            System.out.println("Employee is Present");
+        }
+        else{
+            System.out.println("Employee is Absent");
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
     }
