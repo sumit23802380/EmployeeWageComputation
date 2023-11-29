8ea3bdf86f5f50f9d62da6e41b85be738e82c409
sumit23802380 <sumitjangid1692153@gmail.com>
2023-11-29 22:36:21 +0530

    Added : UC8 ComputeEmployeeWage for multiple companies

--- src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
@@ -7,6 +7,7 @@ public class EmployeeWageComputation {
     private static final int WAGE_PER_HOUR = 20;
     private static final int MAX_WORKING_HOURS = 100;
     private static final int MAX_WORKING_DAYS = 20;
+
     public EmployeeWageComputation(){
 
     }
@@ -43,6 +44,26 @@ public class EmployeeWageComputation {
         System.out.println("Daily Wage is " + dailyWage);
         return new Pair(hoursWorked,dailyWage);
     }
+    private static Pair calculateDailyWage(int wagePerHour){
+        int particularDayAttendance = checkAttendance();
+        int hoursWorked = 0;
+
+        switch (particularDayAttendance){
+            case EMPLOYEE_ABSENT :
+                break;
+            case FULL_TIME_EMPLOYEE:
+                hoursWorked = 8;
+                break;
+            case PART_TIME_EMPLOYEE:
+                hoursWorked = 4;
+                break;
+            default:
+                System.out.println("No Category Defined");
+        }
+        int dailyWage = hoursWorked*wagePerHour;
+        System.out.println("Daily Wage is " + dailyWage);
+        return new Pair(hoursWorked,dailyWage);
+    }
     /*
      *@desc : Calculated the monthly wage of employee
      *@params :
@@ -66,6 +87,32 @@ public class EmployeeWageComputation {
         int totalWageTillMaxWorkingHoursAndDays = 0;
         while (totalHours < MAX_WORKING_HOURS && totalDays < MAX_WORKING_DAYS){
             Pair p = calculateDailyWage();
+            totalHours += p.first;
+            totalDays++;
+            totalWageTillMaxWorkingHoursAndDays += p.second;
+        }
+        System.out.println("Total working hours : " + totalHours);
+        System.out.println("Total working days : " + totalDays);
+        System.out.println("Total Wage till max working hours and days is : " + totalWageTillMaxWorkingHoursAndDays);
+    }
+    public static void calculateMonthlyWageForCompany(int wagePerHour, int maxWorkingDays, int maxWorkingHours) {
+        int monthlyWage = 0;
+        int totalHours = 0;
+        for (int dayNumber = 1;  dayNumber<= maxWorkingDays; dayNumber++) {
+            Pair dailyWage = calculateDailyWage(wagePerHour);
+            if(totalHours + dailyWage.first < maxWorkingHours){
+                monthlyWage += dailyWage.second;
+                totalHours += dailyWage.first;
+            }
+        }
+        System.out.println("Monthly Wage for Employee is :" + monthlyWage);
+    }
+    public static void calculateTillMaxWorkingHoursAndDaysForCompany(int wagePerHour, int maxWorkingDays, int maxWorkingHours) {
+        int totalDays = 0;
+        int totalHours = 0;
+        int totalWageTillMaxWorkingHoursAndDays = 0;
+        while (totalHours < maxWorkingHours && totalDays < maxWorkingDays) {
+            Pair p = calculateDailyWage(wagePerHour);
             totalHours += p.first;
             totalDays++;
             totalWageTillMaxWorkingHoursAndDays += p.second;
--- src/com/bridgelabz/employeewagecomputation/Main.java
+++ src/com/bridgelabz/employeewagecomputation/Main.java
@@ -1,9 +1,8 @@
 package com.bridgelabz.employeewagecomputation;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
         EmployeeWageComputation employeewagecomputation = new EmployeeWageComputation();
-        EmployeeWageComputation.calculateTillMaxWorkingHoursAndDays();
+        EmployeeWageComputation.calculateTillMaxWorkingHoursAndDaysForCompany(25, 22, 110);
     }
 }
